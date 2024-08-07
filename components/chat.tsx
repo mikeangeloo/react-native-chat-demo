e8dbc7d24d53320205d6030ca3368bfb49391331
Miguel Angel Ramirez Lopez <C-MiguelAngel.Ramirez@charter.com>
2024-08-06 20:13:34 -0500

    adding package for websocket

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   View,
   TextInput,
@@ -9,6 +9,7 @@ import {
 import { ChatItem } from "./interfaces/chat-item.interface";
 import { RenderChatItem } from "./chat-item";
 import Styles from "./styles";
+import Socket from "./socket";
 
 type ChatProps = {
   username: string;
@@ -18,6 +19,24 @@ type ChatProps = {
 const Chat = ({ username, image }: ChatProps) => {
   let [chatInput, setChatInput] = useState("");
   let [chatItemList, setChatItemList] = useState<ChatItem[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (Socket.state === "Disconnected") {
+          await Socket.start();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+    Socket.on("ReceiveMessage", (chatItem: ChatItem) => {
+      setChatItemList((chatItemList) => {
+        if (chatItemList.find((i) => i.id === chatItem.id)) return chatItemList;
+        return [...chatItemList, chatItem];
+      });
+    });
+  }, []);
 
   const renderItem: ListRenderItem<ChatItem> = ({ item }) => (
     <RenderChatItem chatItem={item} username={username} />
@@ -39,17 +58,14 @@ const Chat = ({ username, image }: ChatProps) => {
         />
         <Button
           title="Send"
-          onPress={() => {
-            setChatItemList([
-              ...chatItemList,
-              {
-                id: Math.random().toString(36).substring(7),
-                text: chatInput,
-                image: image,
-                timeStamp: Date.now(),
-                by: username,
-              },
-            ]);
+          onPress={async () => {
+            await Socket.invoke("SendMessage", {
+              id: Math.random().toString(36).substring(7),
+              text: chatInput,
+              image: image,
+              timeStamp: Date.now(),
+              by: username,
+            });
             setChatInput("");
           }}
         />
